1dd60d05dd1094c7703cea802b179e77942734c9
janecvo <janecvo@gmail.com>
2022-05-05 16:41:16 -0400

    edit for >12

--- src/CountToEleven.java
+++ src/CountToEleven.java
@@ -2,12 +2,33 @@ import java.util.Scanner;
 public class CountToEleven {
     public static void main (String[] args){
         Scanner scanner = new Scanner (System.in);
+
+        while (true){
         System.out.println("Enter a number less than 11");
         int start = Integer.valueOf(scanner.nextLine());
         int end = 12;
-
+        if (start > end){
+            System.out.println("Your number is greater than 11. Please enter a number less than 11.");
+        }else 
         for (int i = start; i < end; i++){
-            System.out.println(i);
+                        System.out.println(i);
+                }
+                break;
         }
     }
 }
+
+
+
+
+        
+        
+//         if (start >12){
+//             System.out.println("Your number is greater than 11. Please pick another number.");
+//         }else{
+//         for (int i = start; i < end; i++){
+//             System.out.println(i);
+//             }
+//         }
+//     }
+// }
