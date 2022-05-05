8b427ff83f38af52831de8b1c02f3c62e7922e53
janecvo <janecvo@gmail.com>
2022-05-05 13:22:07 -0400

    first commit

---
+++ src/DivideMethod.java
@@ -0,0 +1,10 @@
+public class DivideMethod {
+    public static void main (String [] args){
+        double results = divide(10,3);
+        System.out.println(results);
+    }
+
+    public static double divide(double num1, double num2){
+        return num1/num2;
+    }
+}
